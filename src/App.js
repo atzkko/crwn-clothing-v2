7d87d751ff554d0af43348497a1932bfd03bb7ad
Aleksandar Markovic <atzkko@gmail.com>
2022-06-23 17:43:04 +0200

    88. Setting Up Firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
-import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom';
 
-const Navigation = () => {
-    return (
-        <div>
-            <div>
-                <h1>Navigation bar</h1>
-            </div>
-            <Outlet />
-        </div>
-    )
-}
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.componenet";
 
 const Shop = () => {
     return (
@@ -24,6 +16,7 @@ const App = () => {
           <Route path='/' element={<Navigation />}>
               <Route index element={<Home />} />
               <Route path='shop' element={<Shop />} />
+              <Route path='sign-in' element={<SignIn />} />
           </Route>
       </Routes>
   );
